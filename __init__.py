83039197717a5a3606858412281d2397811eff84
Jeremy Baucherel <jeremy.baucherel@free.fr>
2022-06-26 23:38:53 +0200

    maj

--- __init__.py
+++ __init__.py
@@ -126,82 +126,146 @@ class Version(Base):
     date = sqlalchemy.Column(sqlalchemy.DateTime())
     description = sqlalchemy.Column(StrippedUnicode())
 
-class Auteur(Base):
-    __tablename__ = u"author"
+
+## BIBLIOTHEQUE ##
+
+class Bibliotheque_Auteur(Base):
+    __tablename__ = u"bibliotheque_author"
 
     id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True, autoincrement=True)
     name_author = sqlalchemy.Column(StrippedUnicode())
 
 class AuteurBibliotheque(Base):
-    __tablename__ = u"author_book_list"
+    __tablename__ = u"bibliotheque_author_book_list"
 
-    id_book_list = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("book_list.id"), primary_key=True, autoincrement=True)
-    id_author = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("author.id"), primary_key=True, autoincrement=True)
+    id_book_list = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("bibliotheque_book_list.id"), primary_key=True, autoincrement=True)
+    id_author = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("bibliotheque_author.id"), primary_key=True, autoincrement=True)
 
 class Bibliotheque(Base):
-    __tablename__ = u"book_list"
+    __tablename__ = u"bibliotheque_book_list"
 
     id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True, autoincrement=True)
-    id_type = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("type.id"))
-    id_saga = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("saga.id"))
+    id_categorie = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("bibliotheque_categorie.id"))
+    id_saga = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("bibliotheque_saga.id"))
     number = sqlalchemy.Column(StrippedUnicode())
     name = sqlalchemy.Column(StrippedUnicode())
-    id_book_publishing = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("book_publishing.id"))
-    id_owner = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("owner.id"))
-    id_location = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("location.id"))
+    id_book_publishing = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("bibliotheque_book_publishing.id"))
+    id_owner = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("bibliotheque_owner.id"))
+    id_location = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("bibliotheque_location.id"))
 
-    Type = sqlalchemy.orm.relation("Type", lazy='joined')
-    Saga = sqlalchemy.orm.relation("Saga", lazy='joined')
-    MaisonEdition = sqlalchemy.orm.relation("MaisonEdition", lazy='joined')
-    Proprietaire = sqlalchemy.orm.relation("Proprietaire", lazy='joined')
-    Emplacement = sqlalchemy.orm.relation("Emplacement", lazy='joined')
+    Bibliotheque_Categorie = sqlalchemy.orm.relation("Bibliotheque_Categorie", lazy='joined')
+    Bibliotheque_Saga = sqlalchemy.orm.relation("Bibliotheque_Saga", lazy='joined')
+    Bibliotheque_MaisonEdition = sqlalchemy.orm.relation("Bibliotheque_MaisonEdition", lazy='joined')
+    Bibliotheque_Proprietaire = sqlalchemy.orm.relation("Bibliotheque_Proprietaire", lazy='joined')
+    Bibliotheque_Emplacement = sqlalchemy.orm.relation("Bibliotheque_Emplacement", lazy='joined')
 
-class Type(Base):
-    __tablename__ = u"type"
+class Bibliotheque_Categorie(Base):
+    __tablename__ = u"bibliotheque_categorie"
 
     id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True, autoincrement=True)
-    name_type = sqlalchemy.Column(StrippedUnicode())
+    name_categorie = sqlalchemy.Column(StrippedUnicode())
 
-class Saga(Base):
-    __tablename__ = u"saga"
+class Bibliotheque_Saga(Base):
+    __tablename__ = u"bibliotheque_saga"
 
     id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True, autoincrement=True)
     name_saga = sqlalchemy.Column(StrippedUnicode())
 
-class MaisonEdition(Base):
-    __tablename__ = u"book_publishing"
+class Bibliotheque_MaisonEdition(Base):
+    __tablename__ = u"bibliotheque_book_publishing"
 
     id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True, autoincrement=True)
     name_book_publishing = sqlalchemy.Column(StrippedUnicode())
 
-class Emplacement(Base):
-    __tablename__ = u"location"
+class Bibliotheque_Emplacement(Base):
+    __tablename__ = u"bibliotheque_location"
 
     id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True, autoincrement=True)
     name_location = sqlalchemy.Column(StrippedUnicode())  
 
-class Emprunteur(Base):
-    __tablename__ = u"borrower"
+class Bibliotheque_Emprunteur(Base):
+    __tablename__ = u"bibliotheque_borrower"
 
     id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True, autoincrement=True)
-    id_book_list = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("book_list.id"))
+    id_book_list = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("bibliotheque_book_list.id"))
     borrower = sqlalchemy.Column(StrippedUnicode()) 
     borrowing_date = sqlalchemy.Column(sqlalchemy.DateTime())
 
-class Job(Base):
-    __tablename__ = u"job"
+class Bibliotheque_Job(Base):
+    __tablename__ = u"bibliotheque_job"
 
     id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True, autoincrement=True)
     name_job = sqlalchemy.Column(StrippedUnicode())  
 
-class JobAuteur(Base):
-    __tablename__ = u"job_author"
+class Bibliotheque_JobAuteur(Base):
+    __tablename__ = u"bibliotheque_job_author"
 
-    id_job = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("job.id"), primary_key=True, autoincrement=True)
-    id_author = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("author.id"), primary_key=True, autoincrement=True) 
+    id_job = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("bibliotheque_job.id"), primary_key=True, autoincrement=True)
+    id_author = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("bibliotheque_author.id"), primary_key=True, autoincrement=True) 
 
-class Proprietaire(Base):
-    __tablename__ = u"owner"
+class Bibliotheque_Proprietaire(Base):
+    __tablename__ = u"bibliotheque_owner"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True, autoincrement=True)
+    name_owner = sqlalchemy.Column(StrippedUnicode())  
+
+
+## BLURAY ##
+
+class Bluray(Base):
+    __tablename__ = u"bluray"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True, autoincrement=True)
+    titre = sqlalchemy.Column(StrippedUnicode())
+    id_categorie = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("bluray_categorie.id"))
+    id_saga = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("bluray_saga.id"))
+    number = sqlalchemy.Column(StrippedUnicode())
+    steelbook = sqlalchemy.Column(sqlalchemy.Boolean())
+    id_coffret = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("bluray_coffret.id"))
+    id_owner = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("bluray_owner.id"))
+    id_location = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("bluray_location.id"))
+
+    Bluray_Categorie = sqlalchemy.orm.relation("Bluray_Categorie", lazy='joined')
+    Bluray_Saga = sqlalchemy.orm.relation("Bluray_Saga", lazy='joined')
+    Bluray_Coffret = sqlalchemy.orm.relation("Bluray_Coffret", lazy='joined')
+    Bluray_Proprietaire = sqlalchemy.orm.relation("Bluray_Proprietaire", lazy='joined')
+    Bluray_Emplacement = sqlalchemy.orm.relation("Bluray_Emplacement", lazy='joined')
+
+class Bluray_Coffret(Base):
+    __tablename__ = u"bluray_coffret"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True, autoincrement=True)
+    name_coffret = sqlalchemy.Column(StrippedUnicode())
+
+
+class Bluray_Categorie(Base):
+    __tablename__ = u"bluray_categorie"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True, autoincrement=True)
+    name_categorie = sqlalchemy.Column(StrippedUnicode())
+
+class Bluray_Saga(Base):
+    __tablename__ = u"bluray_saga"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True, autoincrement=True)
+    name_saga = sqlalchemy.Column(StrippedUnicode())
+
+class Bluray_Emplacement(Base):
+    __tablename__ = u"bluray_location"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True, autoincrement=True)
+    name_location = sqlalchemy.Column(StrippedUnicode())  
+
+class Bluray_Emprunteur(Base):
+    __tablename__ = u"bluray_borrower"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True, autoincrement=True)
+    id_bluray = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey("bluray.id"))
+    borrower = sqlalchemy.Column(StrippedUnicode()) 
+    borrowing_date = sqlalchemy.Column(sqlalchemy.DateTime())
+
+class Bluray_Proprietaire(Base):
+    __tablename__ = u"bluray_owner"
 
     id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True, autoincrement=True)
     name_owner = sqlalchemy.Column(StrippedUnicode())  
